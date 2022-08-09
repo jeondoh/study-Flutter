37f401f4eb1372e1d97f51bc64b634cb7ed6d002
jeondoh <jundo1302@gmail.com>
2022-08-09 15:14:48 +0900

    feat: 여백 스타일 widget

--- widget_study/lib/pages/home_page.dart
+++ widget_study/lib/pages/home_page.dart
@@ -10,29 +10,9 @@ class MainHomePage extends StatelessWidget {
           centerTitle: true,
           title: const Text("Flutter AppBar")
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            color: Colors.yellow,
-            width: 100,
-            height: 20,
-            child: const Text('My Home Page1', style: TextStyle(fontSize: 24),),
-          ),
-          Container(
-            color: Colors.green,
-            width: 100,
-            height: 50,
-            child: const Text('My Home Page2', style: TextStyle(fontSize: 24),),
-          ),
-          Container(
-            color: Colors.deepOrangeAccent,
-            width: 100,
-            height: 100,
-            child: const Text('My Home Page3', style: TextStyle(fontSize: 24),),
-          ),
-        ],
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 50, horizontal: 100),
+        color: Colors.yellowAccent,
       ),
     );
   }
