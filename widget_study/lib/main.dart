47cf011501daaccbf7f7ebb5050b795044aa915f
jeondoh <jundo1302@gmail.com>
2022-08-09 17:38:14 +0900

    feat: 정적 이미지 가져오기

--- widget_study/lib/main.dart
+++ widget_study/lib/main.dart
@@ -14,7 +14,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: MainHomePage(),
+      // home: MainHomePage(),
+      home: const _ImageHomePage(),
+    );
+  }
+}
+
+class _ImageHomePage extends StatelessWidget {
+  const _ImageHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/image/asdf.jpg'),
+            Image.asset('assets/image/image2.gif'),
+            Image.asset('assets/image/asdf.jpg'),
+          ],
+        ),
+      )
     );
   }
 }
