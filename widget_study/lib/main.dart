494228d808820e33271ef17237c38191e78b2823
jeondoh <jundo1302@gmail.com>
2022-08-10 17:44:58 +0900

    feat: image_picker 를 활용한 카메라, 사진 추가

--- widget_study/lib/main.dart
+++ widget_study/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:widget_study/pages/image_page.dart';
 import 'package:widget_study/pages/my_home_page.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
@@ -50,7 +51,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(),
+      // home: const MyHomePage(),
+      home: const ImagePage(),
     );
   }
 }
