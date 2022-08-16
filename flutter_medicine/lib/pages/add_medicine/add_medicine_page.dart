11418c55bda5bbdb7d951ed437aca96e54a34a01
jeondoh <jundo1302@gmail.com>
2022-08-16 15:13:23 +0900

    feat: 바텀시트 제작

--- flutter_medicine/lib/pages/add_medicine/add_medicine_page.dart
+++ flutter_medicine/lib/pages/add_medicine/add_medicine_page.dart
@@ -32,66 +32,106 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-        child: Padding(
-          padding: pagePadding,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '어떤 약이에요?',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              const SizedBox(
-                height: largeSpace,
-              ),
-              Center(
-                child: CircleAvatar(
-                  radius: 40,
-                  child: CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      ImagePicker()
-                          .pickImage(source: ImageSource.camera)
-                          .then((xfile) {
-                        if (xfile == null) return;
-                        setState(() {
-                          _pickedImage = File(xfile.path);
-                        });
-                      });
-                    },
-                    child: _pickedImage == null
-                        ? const Icon(
-                            CupertinoIcons.photo_camera_solid,
-                            size: 30,
-                            color: Colors.white,
-                          )
-                        : CircleAvatar(
-                            foregroundImage: FileImage(_pickedImage!),
-                            radius: 40,
-                          ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: pagePadding,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '어떤 약이에요?',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                const SizedBox(
+                  height: largeSpace,
+                ),
+                Center(
+                  child: CircleAvatar(
+                    radius: 40,
+                    child: CupertinoButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (context) {
+                              return SafeArea(
+                                child: Padding(
+                                  padding: pagePadding,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      TextButton(
+                                        onPressed: () {
+                                          ImagePicker()
+                                              .pickImage(
+                                                  source: ImageSource.camera)
+                                              .then((xfile) {
+                                            if (xfile != null) {
+                                              setState(() {
+                                                _pickedImage = File(xfile.path);
+                                              });
+                                            }
+                                            Navigator.maybePop(context);
+                                          });
+                                        },
+                                        child: const Text('카메라로 촬영'),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          ImagePicker()
+                                              .pickImage(
+                                                  source: ImageSource.gallery)
+                                              .then((xfile) {
+                                            if (xfile != null) {
+                                              setState(() {
+                                                _pickedImage = File(xfile.path);
+                                              });
+                                            }
+                                            Navigator.maybePop(context);
+                                          });
+                                        },
+                                        child: const Text('앨범에서 가져오기'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            });
+                      },
+                      child: _pickedImage == null
+                          ? const Icon(
+                              CupertinoIcons.photo_camera_solid,
+                              size: 30,
+                              color: Colors.white,
+                            )
+                          : CircleAvatar(
+                              foregroundImage: FileImage(_pickedImage!),
+                              radius: 40,
+                            ),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: largeSpace + regularSpace,
-              ),
-              Text(
-                '약 이름',
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-              TextFormField(
-                controller: _nameController,
-                maxLength: 20,
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-                style: Theme.of(context).textTheme.bodyText1,
-                decoration: InputDecoration(
-                  hintText: '복용할 약 이름을 기입해주세요.',
-                  hintStyle: Theme.of(context).textTheme.bodyText2,
-                  contentPadding: textFieldContentPadding,
+                const SizedBox(
+                  height: largeSpace + regularSpace,
                 ),
-              ),
-            ],
+                Text(
+                  '약 이름',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                TextFormField(
+                  controller: _nameController,
+                  maxLength: 20,
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.done,
+                  style: Theme.of(context).textTheme.bodyText1,
+                  decoration: InputDecoration(
+                    hintText: '복용할 약 이름을 기입해주세요.',
+                    hintStyle: Theme.of(context).textTheme.bodyText2,
+                    contentPadding: textFieldContentPadding,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
