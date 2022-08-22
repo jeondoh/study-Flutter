b9e46b7a8a349cf7eaf99df625ba3f24e4052b53
jeondoh <jundo1302@gmail.com>
2022-08-22 13:48:33 +0900

    fix: box 이름 직관적이게 변경

--- flutter_medicine/lib/repositories/medicine_history_repository.dart
+++ flutter_medicine/lib/repositories/medicine_history_repository.dart
@@ -7,32 +7,39 @@ import 'package:hive/hive.dart';
 class MedicineHistoryRepository {
   Box<MedicineHistory>? _historyBox;
 
-  Box<MedicineHistory> get medicineBox {
+  Box<MedicineHistory> get historyBox {
     _historyBox ??= Hive.box<MedicineHistory>(MedicineHiveBox.medicineHistory);
     return _historyBox!;
   }
 
   void addHistory(MedicineHistory history) async {
-    int key = await medicineBox.add(history);
+    int key = await historyBox.add(history);
 
     log('[addHistory] add {key:$key} $history');
-    log('result ${medicineBox.values.toList()}');
+    log('result ${historyBox.values.toList()}');
   }
 
   void deleteHistory(int key) async {
-    await medicineBox.delete(key);
+    await historyBox.delete(key);
 
     log('[deleteHistory] delete {key:$key}');
-    log('result ${medicineBox.values.toList()}');
+    log('result ${historyBox.values.toList()}');
+  }
+
+  void deleteAllHistory(Iterable<int> keys) async {
+    await historyBox.deleteAll(keys);
+
+    log('[deleteAllHistory] delete {keys:$keys}');
+    log('result ${historyBox.values.toList()}');
   }
 
   void updateHistory({
     required int key,
     required MedicineHistory history,
   }) async {
-    await medicineBox.put(key, history);
+    await historyBox.put(key, history);
 
     log('[updateHistory] update {key:$key} $history');
-    log('result ${medicineBox.values.toList()}');
+    log('result ${historyBox.values.toList()}');
   }
 }
