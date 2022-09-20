c174bf32d849f5d04396d090b646c91b13b39fc4
jeondoh <jundo1302@gmail.com>
2022-09-20 16:00:02 +0900

    feat: add key on constructor

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: HomeScreen(),
     ),
   );
 }
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
