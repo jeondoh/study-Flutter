5d8765c151878903d2fc9b31be387eb464e4371a
jeondoh <jundo1302@gmail.com>
2022-11-04 23:59:20 +0900

    feat: mypage discover people

---
+++ insta_clone/lib/src/pages/user_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:insta_clone/src/components/avatar_widget.dart';
+
+class UserCard extends StatelessWidget {
+  final String userId;
+  final String description;
+  const UserCard({Key? key, required this.userId, required this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 3),
+      width: 150,
+      height: 220,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        border: Border.all(color: Colors.black12),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            left: 15,
+            right: 15,
+            top: 0,
+            bottom: 0,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const AvatarWidget(
+                  type: AvatarType.TYPE2,
+                  thumbPath:
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fff2lftqIE077pFAKU1Mhbcj8YFvBbMvpA&usqp=CAU',
+                  size: 80,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  userId,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+                ElevatedButton(onPressed: () {}, child: const Text('follow'))
+              ],
+            ),
+          ),
+          Positioned(
+            right: 5,
+            top: 5,
+            child: GestureDetector(
+              onTap: () {},
+              child: const Icon(
+                Icons.close,
+                size: 14,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
