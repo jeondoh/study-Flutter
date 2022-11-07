540b36b78bbf1789fe183f27387255671ab60720
jeondoh <jundo1302@gmail.com>
2022-11-07 17:11:22 +0900

    프로젝트명과 라이브러리명이 같아서 제거후 재생성

--- video_player/android/app/src/main/kotlin/com/study_video/video_player/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.study_video.video_player
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- video_player/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:video_player/screen/home_screen.dart';
-
-void main() {
-  runApp(
-    const MaterialApp(home: HomeScreen()),
-  );
-}
