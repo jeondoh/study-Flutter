c7a5e14db6891ca3eecbc2e0d3ce2292a83e8361
jeondoh <jundo1302@gmail.com>
2022-11-07 22:36:51 +0900

    feat: theme 적용

--- d_day_app/lib/main.dart
+++ d_day_app/lib/main.dart
@@ -4,8 +4,31 @@ import 'screen/home_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomeScreen(),
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 50.0,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
     ),
   );
 }
