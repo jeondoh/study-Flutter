b59d2ead1f39f30feb4fbaaf7995ecc6e4605ac3
jeondoh <jundo1302@gmail.com>
2022-11-07 17:40:34 +0900

    feat: 홈화면 구현

--- vid_player/lib/screen/home_screen.dart
+++ vid_player/lib/screen/home_screen.dart
@@ -6,9 +6,64 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("homeScreen"),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: getBoxDecoration(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            _Logo(),
+            SizedBox(height: 30.0),
+            _AppName(),
+          ],
+        ),
       ),
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xFF000118),
+        ],
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('asset/image/logo.png');
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('VIDEO', style: textStyle),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(fontWeight: FontWeight.w700),
+        ),
+      ],
     );
   }
 }
