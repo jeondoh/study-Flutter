fe0c13fc91215a4726d54e336017988cd8f13642
jeondoh <jundo1302@gmail.com>
2022-11-07 17:01:41 +0900

    init video player project

---
+++ video_player/android/app/src/main/kotlin/com/study_video/video_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.study_video.video_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ video_player/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(home: HomeScreen()),
+  );
+}
