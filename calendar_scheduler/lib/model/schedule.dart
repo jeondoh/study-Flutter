7accbd99002252073d2a82e513310038ab219e01
jeondoh <jundo1302@gmail.com>
2022-11-09 19:03:03 +0900

    feat: database model 정의

---
+++ calendar_scheduler/lib/model/schedule.dart
@@ -0,0 +1,20 @@
+import 'package:drift/drift.dart';
+
+class Schedules extends Table {
+  // PRIMARY KEY
+  IntColumn get id => integer().autoIncrement()();
+  // 내용
+  TextColumn get content => text()();
+  // 일정 날짜
+  DateTimeColumn get date => dateTime()();
+  // 시작 시간
+  IntColumn get startTime => integer()();
+  // 끝 시간
+  IntColumn get endTime => integer()();
+  // Category Color Table ID
+  IntColumn get colorId => integer()();
+  // 생성날짜
+  DateTimeColumn get createAt => dateTime().clientDefault(
+        () => DateTime.now(),
+      )();
+}
