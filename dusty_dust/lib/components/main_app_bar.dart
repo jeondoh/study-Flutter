bf44e5abf5e73a7937adda433f5c6458efaf289d
jeondoh <jundo1302@gmail.com>
2022-11-13 19:46:04 +0900

    feat: drawer(좌측 메뉴바) 디자인

--- dusty_dust/lib/components/main_app_bar.dart
+++ dusty_dust/lib/components/main_app_bar.dart
@@ -17,41 +17,45 @@ class MainAppBar extends StatelessWidget {
       expandedHeight: 500,
       flexibleSpace: FlexibleSpaceBar(
         background: SafeArea(
-          child: Column(
-            children: [
-              Text(
-                '서울',
-                style: ts.copyWith(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w700,
+          child: Container(
+            // kToolbarHeight : appbar 공간의 높이
+            margin: const EdgeInsets.only(top: kToolbarHeight),
+            child: Column(
+              children: [
+                Text(
+                  '서울',
+                  style: ts.copyWith(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              Text(
-                DateTime.now().toString(),
-                style: ts.copyWith(fontSize: 20.0),
-              ),
-              const SizedBox(height: 20.0),
-              Image.asset(
-                'asset/img/mediocre.png',
-                width: MediaQuery.of(context).size.width / 2,
-              ),
-              const SizedBox(height: 20.0),
-              Text(
-                '보통',
-                style: ts.copyWith(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w700,
+                Text(
+                  DateTime.now().toString(),
+                  style: ts.copyWith(fontSize: 20.0),
                 ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                '나쁘지 않네요!',
-                style: ts.copyWith(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w700,
+                const SizedBox(height: 20.0),
+                Image.asset(
+                  'asset/img/mediocre.png',
+                  width: MediaQuery.of(context).size.width / 2,
                 ),
-              ),
-            ],
+                const SizedBox(height: 20.0),
+                Text(
+                  '보통',
+                  style: ts.copyWith(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  '나쁘지 않네요!',
+                  style: ts.copyWith(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -2,14 +2,17 @@ import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
 import '../components/main_app_bar.dart';
+import '../components/main_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: primaryColor,
+      // drawer : 왼쪽 메뉴 생성
+      drawer: MainDrawer(),
       body: CustomScrollView(
         slivers: [
           MainAppBar(),
