920f5b552e2e2dff02104d9f1fb88d9ff33d46bf
jeondoh <jundo1302@gmail.com>
2022-11-13 23:34:16 +0900

    feat: 미세먼지 데이터 모델링

--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -1,13 +1,48 @@
+import 'package:dio/dio.dart';
 import 'package:dusty_dust/components/category_card.dart';
 import 'package:dusty_dust/const/colors.dart';
+import 'package:dusty_dust/const/data.dart';
 import 'package:flutter/material.dart';
 
 import '../components/hourly_card.dart';
 import '../components/main_app_bar.dart';
 import '../components/main_drawer.dart';
+import '../model/stat_model.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    final response = await Dio().get(
+      'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
+      queryParameters: {
+        'serviceKey': serviceKey,
+        'returnType': 'json',
+        'numberOfRows': 30,
+        'pageNo': 1,
+        'itemCode': 'PM10',
+        'dataGubun': 'HOUR',
+        'searchCondition': 'WEEK',
+      },
+    );
+
+    print(
+      response.data['response']['body']['items'].map(
+        (item) => StatModel.fromJson(json: item),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
