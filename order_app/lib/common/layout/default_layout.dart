a3e1fa4db3a986e9aaf9cd96d5fa1fc3e0c1e6ab
jeondoh <jundo1302@gmail.com>
2022-11-15 00:19:26 +0900

    feat: appbar, bottombar 생성

--- order_app/lib/common/layout/default_layout.dart
+++ order_app/lib/common/layout/default_layout.dart
@@ -3,18 +3,43 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
   const DefaultLayout({
     Key? key,
     required this.child,
     this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    }
+    return AppBar(
+      centerTitle: true,
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Text(
+        title!,
+        style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
     );
   }
 }
