d89176e9482c2ebfd06f128df456bfb6837c13b0
jeondoh <jundo1302@gmail.com>
2022-11-16 23:47:43 +0900

    feat: 모델생성, 화면 매핑

--- order_app/lib/restaurant/view/restaurant_screen.dart
+++ order_app/lib/restaurant/view/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:order_app/restaurant/model/restaurant_model.dart';
 
 import '../../common/const/data.dart';
 import '../component/restaurant_card.dart';
@@ -37,18 +38,31 @@ class RestaurantScreen extends StatelessWidget {
               itemCount: snapshot.data!.length,
               itemBuilder: (_, index) {
                 final item = snapshot.data![index];
-
-                return RestaurantCard(
-                  image: Image.network(
-                    'http://$ip${item['thumbUrl']}',
-                    fit: BoxFit.cover,
-                  ),
+                final pItem = RestaurantModel(
+                  id: item['id'],
                   name: item['name'],
+                  thumbUrl: 'http://$ip${item['thumbUrl']}',
                   tags: List<String>.from(item['tags']),
+                  priceRange: RestaurantPriceRange.values.firstWhere(
+                    (element) => element.name == item['priceRange'],
+                  ),
+                  ratings: item['ratings'],
                   ratingsCount: item['ratingsCount'],
                   deliveryTime: item['deliveryTime'],
                   deliveryFee: item['deliveryFee'],
-                  ratings: item['ratings'],
+                );
+
+                return RestaurantCard(
+                  image: Image.network(
+                    pItem.thumbUrl,
+                    fit: BoxFit.cover,
+                  ),
+                  name: pItem.name,
+                  tags: pItem.tags,
+                  ratingsCount: pItem.ratingsCount,
+                  deliveryTime: pItem.deliveryTime,
+                  deliveryFee: pItem.deliveryFee,
+                  ratings: pItem.ratings,
                 );
               },
               separatorBuilder: (_, index) {
