3ac221c1cd11da416a3df27076c97c375376b127
jeondoh <jundo1302@gmail.com>
2022-11-19 22:45:18 +0900

    feat: 상품 매핑

--- order_app/lib/product/component/product_card.dart
+++ order_app/lib/product/component/product_card.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:order_app/common/const/colors.dart';
+import 'package:order_app/restaurant/model/restaurant_detail_model.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard({Key? key}) : super(key: key);
+  final Image image;
+  final String name;
+  final String detail;
+  final int price;
+
+  const ProductCard({
+    Key? key,
+    required this.image,
+    required this.name,
+    required this.detail,
+    required this.price,
+  }) : super(key: key);
+
+  factory ProductCard.fromModel({
+    required RestaurantProductModel model,
+  }) {
+    return ProductCard(
+      image: Image.network(
+        model.imgUrl,
+        width: 110,
+        height: 110,
+        fit: BoxFit.cover,
+      ),
+      name: model.name,
+      detail: model.detail,
+      price: model.price,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,33 +39,30 @@ class ProductCard extends StatelessWidget {
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(8.0),
-            child: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              width: 110,
-              height: 110,
-              fit: BoxFit.cover,
-            ),
+            child: image,
           ),
           const SizedBox(width: 16.0),
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: const [
+              children: [
                 Text(
-                  '떡볶이',
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                  name,
+                  style: const TextStyle(
+                      fontSize: 18.0, fontWeight: FontWeight.w500),
                 ),
                 Text(
-                  '전통 떡볶이의 정석\n맛남',
+                  detail,
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: BODY_TEXT_COLOR, fontSize: 14.0),
+                  style:
+                      const TextStyle(color: BODY_TEXT_COLOR, fontSize: 14.0),
                 ),
                 Text(
-                  '10000',
+                  price.toString(),
                   textAlign: TextAlign.right,
-                  style: TextStyle(
+                  style: const TextStyle(
                     color: PRIMARY_COLOR,
                     fontSize: 12.0,
                     fontWeight: FontWeight.w500,
--- order_app/lib/restaurant/view/restaurant_detail_screen.dart
+++ order_app/lib/restaurant/view/restaurant_detail_screen.dart
@@ -42,7 +42,7 @@ class RestaurantDetailScreen extends StatelessWidget {
             slivers: [
               renderTop(model: item),
               renderLabel(),
-              renderProducts(),
+              renderProducts(products: item.products),
             ],
           );
         },
@@ -65,18 +65,21 @@ class RestaurantDetailScreen extends StatelessWidget {
     );
   }
 
-  SliverPadding renderProducts() {
+  SliverPadding renderProducts(
+      {required List<RestaurantProductModel> products}) {
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       sliver: SliverList(
         delegate: SliverChildBuilderDelegate(
           (context, index) {
-            return const Padding(
-              padding: EdgeInsets.only(top: 16.0),
-              child: ProductCard(),
+            final model = products[index];
+
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard.fromModel(model: model),
             );
           },
-          childCount: 10,
+          childCount: products.length,
         ),
       ),
     );
