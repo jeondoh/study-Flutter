be03e6209e899989b7769c552f3311a4942cc553
jeondoh <jundo1302@gmail.com>
2022-11-20 23:08:23 +0900

    init study riverpod project

---
+++ status_study/android/app/src/main/kotlin/com/study_status/status_study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.study_status.status_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ status_study/lib/layout/screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../default_layout.dart';
+import 'state_provider_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'HomeScreen',
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const StateProviderScreen(),
+                ),
+              );
+            },
+            child: const Text('StateProciderScreen'),
+          ),
+        ],
+      ),
+    );
+  }
+}
