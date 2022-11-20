8cd3af4e04395eb867d767816d6267f5a8025d02
jeondoh <jundo1302@gmail.com>
2022-11-20 23:18:46 +0900

    상태관리 riverpod 알아보기

--- status_study/lib/layout/screen/state_provider_screen.dart
+++ status_study/lib/layout/screen/state_provider_screen.dart
@@ -1,16 +1,71 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:status_study/provider/state_provider_screen.dart';
 
 import '../default_layout.dart';
 
-class StateProviderScreen extends StatelessWidget {
+class StateProviderScreen extends ConsumerWidget {
   const StateProviderScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
     return DefaultLayout(
       title: 'StateProviderScreen',
-      body: Column(
-        children: [],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(provider.toString()),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: const Text('UP'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const _NextScreen(),
+                  ),
+                );
+              },
+              child: const Text('NextScreen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(provider.toString()),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: const Text('UP'),
+            ),
+          ],
+        ),
       ),
     );
   }
--- status_study/lib/main.dart
+++ status_study/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'layout/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomeScreen(),
+    const ProviderScope(
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
     ),
   );
 }
