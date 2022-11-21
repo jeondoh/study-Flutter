4a08ef369e5e459c4e0a2513e45dcbc5902f5a99
jeondoh <jundo1302@gmail.com>
2022-11-21 17:23:00 +0900

    feat: paginate 반환하는 로직 작성

--- order_app/lib/restaurant/provider/restaurant_provider.dart
+++ order_app/lib/restaurant/provider/restaurant_provider.dart
@@ -19,8 +19,42 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     paginate();
   }
 
-  paginate() async {
-    final resp = await repository.paginate();
-    state = resp;
+  void paginate({
+    int fetchCount = 20,
+    // 추가로 데이터 더 가져오기
+    // true - 추가로 데이터 더 가져옴
+    // false - 새로고침 (현재 상태를 덮어씌움)
+    bool fetchMore = false,
+    // 강제로 다시 로딩
+    // true - CursorPaginationLoading()
+    bool forceRefetch = false,
+  }) async {
+    // State 의 상태
+    // 상태가
+    // 1) CursorPagination - 정상적으로 데이터가 있는 상태
+    // 2) CursorPaginationLoading - 데이터가 로딩중인 상태(현재 캐시 없음)
+    // 3) CursorPaginationError - 에러가 있는 상태
+    // 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터 가져올 때
+    // 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청 받았을 때
+
+    // 바로 반환하는 상황
+    // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고있다면)
+    // 2) 로딩중 - fetchMore : true
+    // fetchMore가 아닐때 - 새로고침
+    if (state is CursorPagination && !forceRefetch) {
+      final pState = state as CursorPagination;
+
+      if (!pState.meta.hasMore) {
+        return;
+      }
+    }
+
+    final isLoading = state is CursorPaginationLoading;
+    final isRefetching = state is CursorPaginationRefetching;
+    final isFetchingMore = state is CursorPaginationFetchingMore;
+
+    if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
+      return;
+    }
   }
 }
