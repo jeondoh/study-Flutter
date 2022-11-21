9b379503110fb6958db5917545c04cb2bdc46048
jeondoh <jundo1302@gmail.com>
2022-11-21 18:20:40 +0900

    feat: 홈 화면 pagination 적용

--- order_app/lib/restaurant/view/restaurant_screen.dart
+++ order_app/lib/restaurant/view/restaurant_screen.dart
@@ -6,23 +6,69 @@ import '../../common/model/cursor_pagination_model.dart';
 import '../component/restaurant_card.dart';
 import 'restaurant_detail_screen.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    // 현재 위치가 최대 길이보다 조금 덜되는 위치까지 왔다면
+    // 새로운 데이터 추가요청
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(fetchMore: true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
+    // 첫 로딩
     if (data is CursorPaginationLoading) {
       return const Center(child: CircularProgressIndicator());
     }
+
+    // 에러
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    // CursorPagination
+    // CursorPaginationFetchingMore
+    // CursorPaginationRefetching
     final cp = data as CursorPagination;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
-        itemCount: cp.data.length,
+        controller: controller,
+        itemCount: cp.data.length + 1,
         itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Center(
+                child: data is CursorPaginationFetchingMore
+                    ? const CircularProgressIndicator()
+                    : const Text('last data!!'),
+              ),
+            );
+          }
+
           final pItem = cp.data[index];
 
           return GestureDetector(
