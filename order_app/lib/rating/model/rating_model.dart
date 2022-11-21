dae6dc04a8d63c88d3d1e4958e9da731e9a91fd3
jeondoh <jundo1302@gmail.com>
2022-11-21 23:20:57 +0900

    feat: user, rating 모델 추가

---
+++ order_app/lib/rating/model/rating_model.dart
@@ -0,0 +1,27 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:order_app/user/model/user_model.dart';
+
+import '../../common/utils/utils.dart';
+
+part 'rating_model.g.dart';
+
+@JsonSerializable()
+class RatingModel {
+  final String id;
+  final UserModel user;
+  final int rating;
+  final String content;
+  @JsonKey(fromJson: DataUtils.listPathsToUrls)
+  final List<String> imgUrls;
+
+  RatingModel({
+    required this.id,
+    required this.user,
+    required this.rating,
+    required this.content,
+    required this.imgUrls,
+  });
+
+  factory RatingModel.fromJson(Map<String, dynamic> json) =>
+      _$RatingModelFromJson(json);
+}
