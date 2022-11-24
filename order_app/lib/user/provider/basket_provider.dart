3b9d3e5b1224905c78ece4dc5eb30347e0ec4be8
jeondoh <jundo1302@gmail.com>
2022-11-25 00:46:08 +0900

    feat: Optimistic Response 사용하여 장바구니 구현

--- order_app/lib/user/provider/basket_provider.dart
+++ order_app/lib/user/provider/basket_provider.dart
@@ -2,14 +2,34 @@ import 'package:collection/collection.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:order_app/product/model/product_model.dart';
 import 'package:order_app/user/model/basket_item_model.dart';
+import 'package:order_app/user/model/patch_basket_body.dart';
+import 'package:order_app/user/repository/user_me_repository.dart';
 
 final basketProvier =
     StateNotifierProvider<BasketProvider, List<BasketItemModel>>((ref) {
-  return BasketProvider();
+  final repository = ref.watch(userMeRepositoryProvider);
+  return BasketProvider(repository: repository);
 });
 
 class BasketProvider extends StateNotifier<List<BasketItemModel>> {
-  BasketProvider() : super([]);
+  final UserMeRepository repository;
+
+  BasketProvider({required this.repository}) : super([]);
+
+  Future<void> patchBasket() async {
+    await repository.patchBasket(
+      body: PatchBasketBody(
+        basket: state
+            .map(
+              (e) => PatchBasketBodyBasket(
+                productId: e.product.id,
+                count: e.count,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
 
   Future<void> addToBasket({
     required ProductModel product,
@@ -29,6 +49,10 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
       // 장바구니에 상품을 추가한다.
       state = [...state, BasketItemModel(product: product, count: 1)];
     }
+    // Optimistic Response (긍정적 응답)
+    // 응답이 성공할거라고 가정하고 상태를 먼저 업데이트함
+    // 상태를 먼저 업데이트 함으로써 UX 적으로 앱이 빨라져 보이는 효과를 줄 수 있음
+    await patchBasket();
   }
 
   Future<void> removeFromBasket({
@@ -64,5 +88,6 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
               : e)
           .toList();
     }
+    await patchBasket();
   }
 }
