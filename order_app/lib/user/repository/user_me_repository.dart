ee710c5c1107f27cfceb5d9be1cd22e98d26dae9
jeondoh <jundo1302@gmail.com>
2022-11-24 19:14:21 +0900

    feat: 장바구니 모델 추가

--- order_app/lib/user/repository/user_me_repository.dart
+++ order_app/lib/user/repository/user_me_repository.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart' hide Headers;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:order_app/user/model/basket_item_model.dart';
+import 'package:order_app/user/model/patch_basket_body.dart';
 import 'package:retrofit/retrofit.dart';
 
 import '../../common/const/data.dart';
@@ -18,8 +20,16 @@ abstract class UserMeRepository {
   factory UserMeRepository(Dio dio, {String baseUrl}) = _UserMeRepository;
 
   @GET('/')
-  @Headers({
-    'accessToken': 'true',
-  })
+  @Headers({'accessToken': 'true'})
   Future<UserModel> getMe();
+
+  @GET('/basket')
+  @Headers({'accessToken': 'true'})
+  Future<List<BasketItemModel>> getBasket();
+
+  @PATCH('/basket')
+  @Headers({'accessToken': 'true'})
+  Future<List<BasketItemModel>> patchBasket({
+    @Body() required PatchBasketBody body,
+  });
 }
