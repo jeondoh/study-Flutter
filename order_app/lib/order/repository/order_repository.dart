62645562197590741c545297474f6dd8288d59c5
jeondoh <jundo1302@gmail.com>
2022-11-25 13:05:45 +0900

    feat: 주문 repository 생

---
+++ order_app/lib/order/repository/order_repository.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:order_app/common/dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../../common/const/data.dart';
+import '../model/order_model.dart';
+import '../model/post_order_body.dart';
+
+part 'order_repository.g.dart';
+
+final orderRepositoryProvider = Provider<OrderRepository>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+    return OrderRepository(dio, baseUrl: 'http://$ip/order');
+  },
+);
+
+@RestApi()
+abstract class OrderRepository {
+  factory OrderRepository(Dio dio, {String baseUrl}) = _OrderRepository;
+
+  @POST('/')
+  @Headers({'accessToken': 'true'})
+  Future<OrderModel> postOrder({
+    @Body() required PostOrderBody body,
+  });
+}
