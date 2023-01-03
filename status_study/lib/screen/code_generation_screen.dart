8831f419fd096690c6ad8725db758c47d323580d
jeondoh <jundo1302@gmail.com>
2023-01-03 14:20:38 +0900

    feat: riverpod 코드제너레이션을 사용해 family, stateNotifier 구현

---
+++ status_study/lib/screen/code_generation_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:status_study/layout/default_layout.dart';
+import 'package:status_study/provider/code_generation_provider.dart';
+
+class CodeGenerationScreen extends ConsumerWidget {
+  const CodeGenerationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state1 = ref.watch(gStateProvider);
+    final state2 = ref.watch(gStateFutureProvider);
+    final state3 = ref.watch(gStateFuture2Provider);
+    final state4 = ref.watch(
+      gStateMultiplyProvider(
+        number1: 10,
+        number2: 20,
+      ),
+    );
+    final state5 = ref.watch(gStateNotifierProvider);
+
+    return DefaultLayout(
+      title: 'CodeGenerationScreen',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('State1 : $state1'),
+          state2.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => const Center(child: CircularProgressIndicator()),
+          ),
+          state3.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => const Center(child: CircularProgressIndicator()),
+          ),
+          Text('State4 : $state4'),
+          Text('State5 : $state5'),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(gStateNotifierProvider.notifier).increment();
+                },
+                child: const Text('Increment'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(gStateNotifierProvider.notifier).decrement();
+                },
+                child: const Text('Decrement'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
