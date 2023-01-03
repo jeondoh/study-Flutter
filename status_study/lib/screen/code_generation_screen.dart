e453783b3192d0c0aefd9bb7ecf79dd543a3caf8
jeondoh <jundo1302@gmail.com>
2023-01-03 14:47:01 +0900

    feat: riverpod Consumer 위젯 사용해보기

--- status_study/lib/screen/code_generation_screen.dart
+++ status_study/lib/screen/code_generation_screen.dart
@@ -8,6 +8,7 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
     final state1 = ref.watch(gStateProvider);
     final state2 = ref.watch(gStateFutureProvider);
     final state3 = ref.watch(gStateFuture2Provider);
@@ -17,7 +18,6 @@ class CodeGenerationScreen extends ConsumerWidget {
         number2: 20,
       ),
     );
-    final state5 = ref.watch(gStateNotifierProvider);
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
@@ -50,7 +50,26 @@ class CodeGenerationScreen extends ConsumerWidget {
             loading: () => const Center(child: CircularProgressIndicator()),
           ),
           Text('State4 : $state4'),
-          Text('State5 : $state5'),
+          // 리랜더링이 안되기 위해 새로운 위젯을 생성해야 한다..
+          // _StateFiveWidget(),
+          // Consumer를 이용하면 매번 새로운 위젯을 생성하지 않아도 된다.
+          // 원하는 위젯만 랜더링 할 수 있게 설계 가능
+          Consumer(
+            builder: (context, ref, child) {
+              final state5 = ref.watch(gStateNotifierProvider);
+              return Row(
+                children: [
+                  Text('state5: $state5'),
+                  // 아래 child 의 값을 가져올 수 있음
+                  // 새로 랜더링 하는 위젯이 부분적일 때 사용한다.
+                  // child 는 단 한번만 랜더링됨
+                  // 그 외 builder 내에 있는 아이들은 변경될 때 마다 랜더링
+                  child!,
+                ],
+              );
+            },
+            child: const Text('Consumer Child'),
+          ),
           Row(
             children: [
               ElevatedButton(
@@ -66,9 +85,27 @@ class CodeGenerationScreen extends ConsumerWidget {
                 child: const Text('Decrement'),
               ),
             ],
-          )
+          ),
+          // invalidate()
+          // 유효하지 않게 하다
+          ElevatedButton(
+            onPressed: () {
+              ref.invalidate(gStateNotifierProvider);
+            },
+            child: const Text('Invalidate'),
+          ),
         ],
       ),
     );
+  }
+}
+
+class _StateFiveWidget extends ConsumerWidget {
+  const _StateFiveWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state5 = ref.watch(gStateNotifierProvider);
+    return Text('State5 : $state5');
   }
 }
