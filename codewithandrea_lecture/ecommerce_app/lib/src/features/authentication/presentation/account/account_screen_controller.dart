05b2ec3770e57e1a8c99f6a63a466b3e66eff986
jeondoh <jundo1302@gmail.com>
2023-03-27 20:40:39 +0900

    refactor: change try catch to asyncValue.guard method

--- codewithandrea_lecture/ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ codewithandrea_lecture/ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,28 +1,23 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+class AccountScreenController extends StateNotifier<AsyncValue> {
   AccountScreenController({
     required this.authRepository,
-  }) : super(const AsyncValue<void>.data(null));
+  }) : super(const AsyncValue.data(null));
 
   final FakeAuthRepository authRepository;
 
   Future<bool> signOut() async {
-    try {
-      state = const AsyncValue<void>.loading();
-      await authRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-      return true;
-    } catch (e, stack) {
-      state = AsyncValue<void>.error(e, stack);
-      return false;
-    }
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+    return state.hasError == false;
   }
 }
 
-final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
-    AccountScreenController, AsyncValue<void>>((ref) {
+final accountScreenControllerProvider =
+    StateNotifierProvider.autoDispose<AccountScreenController, AsyncValue>(
+        (ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return AccountScreenController(authRepository: authRepository);
 });
