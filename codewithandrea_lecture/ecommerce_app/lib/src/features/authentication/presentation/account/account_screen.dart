f625939f99f9283ea5b83bb6fea3d4f7a5232466
jeondoh <jundo1302@gmail.com>
2023-03-28 23:42:39 +0900

    feat: auth check to use go router redirect, refreshListenable argument

--- codewithandrea_lecture/ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
+++ codewithandrea_lecture/ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
@@ -41,12 +41,9 @@ class AccountScreen extends ConsumerWidget {
                       defaultActionText: 'Logout'.hardcoded,
                     );
                     if (logout == true) {
-                      final success = await ref
+                      ref
                           .read(accountScreenControllerProvider.notifier)
                           .signOut();
-                      if (success) {
-                        navigator.pop();
-                      }
                     }
                   },
           ),
--- codewithandrea_lecture/ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ codewithandrea_lecture/ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -8,10 +8,9 @@ class AccountScreenController extends StateNotifier<AsyncValue> {
 
   final FakeAuthRepository authRepository;
 
-  Future<bool> signOut() async {
+  Future<void> signOut() async {
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() => authRepository.signOut());
-    return state.hasError == false;
   }
 }
 
