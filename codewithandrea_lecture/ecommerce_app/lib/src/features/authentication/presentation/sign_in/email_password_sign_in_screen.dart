68900959113abf02a5c1351f1c478a4717d9e5f3
jeondoh <jundo1302@gmail.com>
2023-03-31 16:43:57 +0900

    feat: implement cartService class

--- codewithandrea_lecture/ecommerce_app/lib/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart
+++ codewithandrea_lecture/ecommerce_app/lib/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart
@@ -1,15 +1,16 @@
 import 'package:ecommerce_app/src/common_widgets/custom_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_scrollable_card.dart';
-import 'package:ecommerce_app/src/common_widgets/responsive_scrollable_card.dart';
-import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/string_validators.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/servicce_app/src/common_widgets/primary_button.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'email_password_sign_in_controller.dart';
 
 /// Email & password sign in screen.
 /// Wraps the [EmailPasswordSignInContents] widget below with a [Scaffold] and
--- codewithandrea_lecture/ecommerce_app/lib/src/features/cart/data/local/local_cart_repository.dart
+++ codewithandrea_lecture/ecommerce_app/lib/src/features/cart/data/local/local_cart_repository.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// API for reading, watching and writing local cart data (guest user)
 abstract class LocalCartRepository {
@@ -11,6 +11,5 @@ abstract class LocalCartRepository {
 }
 
 final localCartRepositoryProvider = Provider<LocalCartRepository>((ref) {
-  // * Override this in the main method
   throw UnimplementedError();
 });
