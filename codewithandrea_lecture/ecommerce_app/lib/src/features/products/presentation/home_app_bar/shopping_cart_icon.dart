7f40ab63b22bafb2f9d69199b049887a6f461505
jeondoh <jundo1302@gmail.com>
2023-04-03 20:08:43 +0900

    feat: calculate and showing the cart items count, price

--- codewithandrea_lecture/ecommerce_app/lib/src/features/products/presentation/home_app_bar/shopping_cart_icon.dart
+++ codewithandrea_lecture/ecommerce_app/lib/src/features/products/presentation/home_app_bar/shopping_cart_icon.dart
@@ -1,6 +1,7 @@
+import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,8 +14,8 @@ class ShoppingCartIcon extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: Read from data source
-    const cartItemsCount = 3;
+    final cartItemsCount = ref.watch(cartItemsProvider);
+
     return Stack(
       children: [
         Center(
@@ -25,7 +26,7 @@ class ShoppingCartIcon extends ConsumerWidget {
           ),
         ),
         if (cartItemsCount > 0)
-          const Positioned(
+          Positioned(
             top: Sizes.p4,
             right: Sizes.p4,
             child: ShoppingCartIconBadge(itemsCount: cartItemsCount),
