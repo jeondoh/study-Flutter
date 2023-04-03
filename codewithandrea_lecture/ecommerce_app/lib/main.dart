683e428c1006017b2655e2597a601927fd491bae
jeondoh <jundo1302@gmail.com>
2023-04-03 20:48:15 +0900

    feat: register CartSyncService with providerContainer when the app start

--- codewithandrea_lecture/ecommerce_app/lib/main.dart
+++ codewithandrea_lecture/ecommerce_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_sync_service.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/sembast_cart_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
@@ -17,12 +18,16 @@ void main() async {
   registerErrorHandlers();
 
   final localCartRepository = await SembastCartRepository.makeDefault();
+  final container = ProviderContainer(
+    overrides: [
+      localCartRepositoryProvider.overrideWithValue(localCartRepository),
+    ],
+  );
+  container.read(cartSyncServiceProvider);
   // * Entry point of the app
   runApp(
-    ProviderScope(
-      overrides: [
-        localCartRepositoryProvider.overrideWithValue(localCartRepository),
-      ],
+    UncontrolledProviderScope(
+      container: container,
       child: const MyApp(),
     ),
   );
