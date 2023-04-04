77bf4487da07a29bd9db97f85e2a8a3f507268c5
jeondoh <jundo1302@gmail.com>
2023-04-04 19:00:19 +0900

    feat: implement leaveReviewController and submitting form data

--- codewithandrea_lecture/ecommerce_app/lib/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart
+++ codewithandrea_lecture/ecommerce_app/lib/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart
@@ -1,13 +1,14 @@
-import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
+import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
+import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/constants/breakpoints.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/reviews/domain/review.dart';
+import 'package:ecommerce_app/src/features/reviews/presentation/leave_review_screen/leave_review_controller.dart';
 import 'package:ecommerce_app/src/features/reviews/presentation/product_reviews/product_rating_bar.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
-import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
-import 'package:ecommerce_app/src/constants/app_sizes.dart';
-import 'package:ecommerce_app/src/features/reviews/domain/review.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LeaveReviewScreen extends StatelessWidget {
@@ -63,6 +64,12 @@ class _LeaveReviewFormState extends ConsumerState<LeaveReviewForm> {
 
   @override
   Widget build(BuildContext context) {
+    ref.listen<AsyncValue>(
+      leaveReviewControllerProvider,
+      (_, state) => state.showAlertDialogOnError(context),
+    );
+
+    final state = ref.watch(leaveReviewControllerProvider);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -94,11 +101,15 @@ class _LeaveReviewFormState extends ConsumerState<LeaveReviewForm> {
         PrimaryButton(
           text: 'Submit'.hardcoded,
           // TODO: Loading state
-          isLoading: false,
-          onPressed: _rating == 0
+          isLoading: state.isLoading,
+          onPressed: state.isLoading || _rating == 0
               ? null
-              // TODO: submit review
-              : () => showNotImplementedAlertDialog(context: context),
+              : () =>
+                  ref.read(leaveReviewControllerProvider.notifier).submitReview(
+                        productId: widget.productId,
+                        rating: _rating,
+                        comment: _controller.text,
+                      ),
         )
       ],
     );
