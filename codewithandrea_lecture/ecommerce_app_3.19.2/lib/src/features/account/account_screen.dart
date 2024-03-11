bfa0a34053ce459e1249194133e299e66ba3bda1
jeondoh <jundo1302@gmail.com>
2024-03-11 17:51:04 +0900

    GoRouter - route pop

--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/account/account_screen.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/account/account_screen.dart
@@ -1,10 +1,11 @@
+import 'package:ecommerce_app/src/common_widgets/action_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
+import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/models/app_user.dart';
 import 'package:flutter/material.dart';
-import 'package:ecommerce_app/src/common_widgets/action_text_button.dart';
-import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
-import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:go_router/go_router.dart';
 
 /// Simple account screen showing some user info and a logout button.
 class AccountScreen extends StatelessWidget {
@@ -23,17 +24,17 @@ class AccountScreen extends StatelessWidget {
               // * Get the navigator beforehand to prevent this warning:
               // * Don't use 'BuildContext's across async gaps.
               // * More info here: https://youtu.be/bzWaMpD1LHY
-              // final navigator = Navigator.of(context);
-              // final logout = await showAlertDialog(
-              //   context: context,
-              //   title: 'Are you sure?'.hardcoded,
-              //   cancelActionText: 'Cancel'.hardcoded,
-              //   defaultActionText: 'Logout'.hardcoded,
-              // );
-              // if (logout == true) {
-              //   // TODO: Sign out the user.
-              //   navigator.pop();
-              // }
+              final goRouter = GoRouter.of(context);
+              final logout = await showAlertDialog(
+                context: context,
+                title: 'Are you sure?'.hardcoded,
+                cancelActionText: 'Cancel'.hardcoded,
+                defaultActionText: 'Logout'.hardcoded,
+              );
+              if (logout == true) {
+                // TODO: Sign out the user.
+                goRouter.pop();
+              }
             },
           ),
         ],
--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/sign_in/email_password_sign_in_screen.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/sign_in/email_password_sign_in_screen.dart
@@ -1,18 +1,20 @@
-import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:ecommerce_app/src/common_widgets/custom_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_scrollable_card.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:ecommerce_app/src/features/sign_in/string_validators.dart';
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 
 /// Email & password sign in screen.
 /// Wraps the [EmailPasswordSignInContents] widget below with a [Scaffold] and
 /// [AppBar] with a title.
 class EmailPasswordSignInScreen extends StatelessWidget {
   const EmailPasswordSignInScreen({super.key, required this.formType});
+
   final EmailPasswordSignInFormType formType;
 
   // * Keys for testing using find.byKey()
@@ -25,7 +27,7 @@ class EmailPasswordSignInScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Sign In'.hardcoded)),
       body: EmailPasswordSignInContents(
         formType: formType,
-        onSignedIn: () => Navigator.of(context).pop(),
+        onSignedIn: () => context.pop(),
       ),
     );
   }
@@ -40,10 +42,12 @@ class EmailPasswordSignInContents extends StatefulWidget {
     this.onSignedIn,
     required this.formType,
   });
+
   final VoidCallback? onSignedIn;
 
   /// The default form type to use.
   final EmailPasswordSignInFormType formType;
+
   @override
   State<EmailPasswordSignInContents> createState() =>
       _EmailPasswordSignInContentsState();
@@ -57,6 +61,7 @@ class _EmailPasswordSignInContentsState
   final _passwordController = TextEditingController();
 
   String get email => _emailController.text;
+
   String get password => _passwordController.text;
 
   // local variable used to apply AutovalidateMode.onUserInteraction and show
@@ -64,6 +69,7 @@ class _EmailPasswordSignInContentsState
   // For more details on how this is implemented, see:
   // https://codewithandrea.com/articles/flutter-text-field-form-validation/
   var _submitted = false;
+
   // local variable representing the form type and loading state
   late var _state =
       EmailPasswordSignInState(formType: widget.formType, isLoading: false);
