5f611a5fb8aa316ea80b3e271bfa4bc7cd637381
jeondoh <jundo1302@gmail.com>
2024-03-13 14:31:27 +0900

    family modifier

--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/products/data/fake_products_repository.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/products/data/fake_products_repository.dart
@@ -13,12 +13,14 @@ class FakeProductsRepository {
     return _products.firstWhere((product) => product.id == id);
   }
 
-  Future<List<Product>> fetchProductsList() {
+  Future<List<Product>> fetchProductsList() async {
+    await Future.delayed(const Duration(seconds: 2));
     return Future.value(_products);
   }
 
-  Stream<List<Product>> watchProductsList() {
-    return Stream.value(_products);
+  Stream<List<Product>> watchProductsList() async* {
+    await Future.delayed(const Duration(seconds: 2));
+    yield _products;
   }
 
   Stream<Product?> watchProduct(String id) {
@@ -39,4 +41,9 @@ final productsListStreamProvider = StreamProvider<List<Product>>((ref) {
 final productsListFutureProvider = FutureProvider<List<Product>>((ref) {
   final productsRepository = ref.watch(productsRepositoryProvider);
   return productsRepository.fetchProductsList();
+});
+
+final productProvider = StreamProvider.family<Product?, String>((ref, id) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+  return productsRepository.watchProduct(id);
 });
