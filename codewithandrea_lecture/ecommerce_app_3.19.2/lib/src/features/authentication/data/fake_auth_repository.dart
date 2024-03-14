a199de8ec91f42b64f7b44b04d3dd7422b1fab12
jeondoh <jundo1302@gmail.com>
2024-03-14 17:41:25 +0900

    testing fakeAuthRepository

--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/authentication/data/fake_auth_repository.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,8 +1,12 @@
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_app/src/utils/delay.dart';
 import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeAuthRepository {
+  FakeAuthRepository({this.addDelay = true});
+
+  final bool addDelay;
   final _authState = InMemoryStore<AppUser?>(null);
 
   Stream<AppUser?> authStateChanges() => _authState.stream;
@@ -10,18 +14,14 @@ class FakeAuthRepository {
   AppUser? get currentUser => _authState.value;
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
-    await Future.delayed(const Duration(seconds: 3));
-
-    if (currentUser == null) {
-      _createNewUser(email);
-    }
+    await delay(addDelay);
+    _createNewUser(email);
   }
 
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
-    if (currentUser == null) {
-      _createNewUser(email);
-    }
+    await delay(addDelay);
+    _createNewUser(email);
   }
 
   Future<void> signOut() async {
--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/authentication/domain/app_user.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/authentication/domain/app_user.dart
@@ -4,6 +4,23 @@ class AppUser {
     required this.uid,
     this.email,
   });
+
   final String uid;
   final String? email;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AppUser &&
+          runtimeType == other.runtimeType &&
+          uid == other.uid &&
+          email == other.email;
+
+  @override
+  int get hashCode => uid.hashCode ^ email.hashCode;
+
+  @override
+  String toString() {
+    return 'AppUser{uid: $uid, email: $email}';
+  }
 }
