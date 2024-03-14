08db001ab777004c5be0267b2b078d700544b373
jeondoh <jundo1302@gmail.com>
2024-03-14 18:33:11 +0900

    testing the accountScreenController type matchers

--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,9 +1,9 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue> {
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   AccountScreenController({required this.authRepository})
-      : super(const AsyncValue.data(null));
+      : super(const AsyncValue<void>.data(null));
 
   final FakeAuthRepository authRepository;
 
@@ -18,13 +18,15 @@ class AccountScreenController extends StateNotifier<AsyncValue> {
     //   return false;
     // }
     // try-catch 없이 AsyncValue.guard() 사용
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() => authRepository.signOut());
+
+    // AsyncValue.loading = AsyncLoading
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard<void>(() => authRepository.signOut());
   }
 }
 
 final accountScreenControllerProvider =
-    StateNotifierProvider<AccountScreenController, AsyncValue>((ref) {
+    StateNotifierProvider<AccountScreenController, AsyncValue<void>>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return AccountScreenController(authRepository: authRepository);
 });
