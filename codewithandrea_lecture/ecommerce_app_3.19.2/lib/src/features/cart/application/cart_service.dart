35f79126ca6de8a2ebbe935bfce3d8415a7bbfa2
jeondoh <jundo1302@gmail.com>
2024-03-18 14:38:19 +0900

    updating cartService class to read dependencies using ref

--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/cart/application/cart_service.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/cart/application/cart_service.dart
@@ -8,31 +8,25 @@ import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartService {
-  CartService({
-    required this.authRepository,
-    required this.localCartRepository,
-    required this.remoteCartRepository,
-  });
+  CartService(this.ref);
 
-  final FakeAuthRepository authRepository;
-  final LocalCartRepository localCartRepository;
-  final RemoteCartRepository remoteCartRepository;
+  final Ref ref;
 
   Future<Cart> _fetchCart() {
-    final user = authRepository.currentUser;
+    final user = ref.read(authRepositoryProvider).currentUser;
     if (user != null) {
-      return remoteCartRepository.fetchCart(user.uid);
+      return ref.read(remoteCartRepositoryProvider).fetchCart(user.uid);
     } else {
-      return localCartRepository.fetchCart();
+      return ref.read(localCartRepositoryProvider).fetchCart();
     }
   }
 
   Future<void> _setCart(Cart cart) async {
-    final user = authRepository.currentUser;
+    final user = ref.read(authRepositoryProvider).currentUser;
     if (user != null) {
-      await remoteCartRepository.setCart(user.uid, cart);
+      await ref.read(remoteCartRepositoryProvider).setCart(user.uid, cart);
     } else {
-      await localCartRepository.setCart(cart);
+      await ref.read(localCartRepositoryProvider).setCart(cart);
     }
   }
 
@@ -56,9 +50,5 @@ class CartService {
 }
 
 final cartProvider = Provider<CartService>((ref) {
-  return CartService(
-    authRepository: ref.watch(authRepositoryProvider),
-    localCartRepository: ref.watch(localCartRepositoryProvider),
-    remoteCartRepository: ref.watch(remoteCartRepositoryProvider),
-  );
+  return CartService(ref);
 });
