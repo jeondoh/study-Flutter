71d28e66d2f2ab72ac842822debdd5ba944b1c6a
jeondoh <jundo1302@gmail.com>
2024-03-18 11:42:12 +0900

    add files to make shopping cart

--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/cart/domain/mutable_cart.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/cart/domain/mutable_cart.dart
@@ -4,6 +4,12 @@ import 'package:ecommerce_app/src/features/products/domain/product.dart';
 
 /// Helper extension used to mutate the items in the shopping cart.
 extension MutableCart on Cart {
+  Cart setItem(Item item) {
+    final copy = Map<ProductID, int>.from(items);
+    copy[item.productId] = item.quantity;
+    return Cart(copy);
+  }
+
   Cart addItem(Item item) {
     final copy = Map<ProductID, int>.from(items);
     if (copy.containsKey(item.productId)) {
