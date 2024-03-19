49eebc4fad8f2862b55a6e92c32d2f8bd7e57623
jeondoh <jundo1302@gmail.com>
2024-03-19 15:12:23 +0900

    implement the logic inside the CartSyncService

--- codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/cart/application/cart_sync_service.dart
+++ codewithandrea_lecture/ecommerce_app_3.19.2/lib/src/features/cart/application/cart_sync_service.dart
@@ -1,6 +1,13 @@
+import 'dart:math';
+
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:ecommerce_app/src/features/cart/domain/item.dart';
+import 'package:ecommerce_app/src/features/cart/domain/mutable_cart.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartSyncService {
@@ -16,9 +23,57 @@ class CartSyncService {
       final previousUser = previous?.value;
       final user = next.value;
       if (previousUser == null && user != null) {
-        debugPrint('signed in: ${user.uid}');
+        _moveItemsToRemoteCart(user.uid);
       }
     });
+  }
+
+  Future<void> _moveItemsToRemoteCart(String uid) async {
+    try {
+      // get the local cart data
+      final localCartRepository = ref.read(localCartRepositoryProvider);
+      final localCart = await localCartRepository.fetchCart();
+      // localCart -> remoteCart
+      if (localCart.items.isNotEmpty) {
+        // get the remote cart data
+        final remoteCartRepository = ref.read(remoteCartRepositoryProvider);
+        final remoteCart = await remoteCartRepository.fetchCart(uid);
+        final localItemsToAdd =
+            await _getLocalItemsToAdd(localCart, remoteCart);
+        // local 카트에 있는 아이템 > remote cart로 이전
+        final updatedRemoteCart = remoteCart.addItems(localItemsToAdd);
+        await remoteCartRepository.setCart(uid, updatedRemoteCart);
+        // local cart 아이템 삭제
+        await localCartRepository.setCart(const Cart());
+      }
+    } catch (e) {
+      // todo: handle error
+    }
+  }
+
+  Future<List<Item>> _getLocalItemsToAdd(
+      Cart localCart, Cart remoteCart) async {
+    final productsRepository = ref.read(productsRepositoryProvider);
+    final products = await productsRepository.fetchProductsList();
+    final localItemsToAdd = <Item>[];
+
+    for (final localItem in localCart.items.entries) {
+      final productId = localItem.key;
+      final localQuantity = localItem.value;
+      final remoteQuantity = remoteCart.items[productId] ?? 0;
+      final product = products.firstWhere((product) => product.id == productId);
+      final cappedLocalQuantity = min(
+        localQuantity,
+        product.availableQuantity - remoteQuantity,
+      );
+      if (cappedLocalQuantity > 0) {
+        localItemsToAdd.add(Item(
+          productId: productId,
+          quantity: cappedLocalQuantity,
+        ));
+      }
+    }
+    return localItemsToAdd;
   }
 }
 
